2305ef65f63d2ba42c3397bff787f6b545c66ecd
Teo Dragovic <teo.dragovic@gmail.com>
2018-07-07 11:25:34 +0200

    Site updated at 2018-07-07 09:25:34 UTC

--- main.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var e,t;(e=".js-mail",t=document.querySelectorAll(e),[].slice.call(t)).forEach(function(e){e.addEventListener("click",function(e){var t=e.target;if(""===t.href){var r=t.innerText.split("").reverse().join("");return t.setAttribute("href","mailto:"+r)}},{once:!0})})}();
-//# sourceMappingURL=main.js.map
