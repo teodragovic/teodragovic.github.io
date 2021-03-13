4934c9e9c3367d8a6ca93ee262aad4b8d29496ad
Teo Dragovic <teo.dragovic@gmail.com>
2021-03-13 14:52:51 +0100

    formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,65 +1,66 @@
-const { DateTime } = require("luxon");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+
+const { DateTime } = require('luxon');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addWatchTarget("styles/");
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addWatchTarget('styles/');
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
-  eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+    eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy LL dd");
-  });
+    eleventyConfig.addFilter('readableDate', dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy LL dd');
+    });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
 
-  eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy('img');
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    /* Markdown Overrides */
+    const markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkClass: 'direct-link',
+        permalinkSymbol: '#'
+    });
+    eleventyConfig.setLibrary('md', markdownLibrary);
 
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    open: true,
-    ui: false,
-    ghostMode: false
-  });
+    // Browsersync Overrides
+    eleventyConfig.setBrowserSyncConfig({
+        open: true,
+        ui: false,
+        ghostMode: false
+    });
 
-  return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    return {
+        templateFormats: [
+            'md',
+            'njk',
+            'html',
+            'liquid'
+        ],
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+        markdownTemplateEngine: 'liquid',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
 
-    // These are all optional, defaults are shown:
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+        // These are all optional, defaults are shown:
+        dir: {
+            input: '.',
+            includes: '_includes',
+            data: '_data',
+            output: '_site'
+        }
+    };
 };
